2726f9cd11c85080384d06283d1c964e7b052deb
mia <329625584@qq.com>
2019-06-01 14:03:13 +0800

    基本注册登录

--- app/src/main/java/com/example/myapplication/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/ui/login/LoginActivity.java
@@ -38,8 +38,8 @@ public class LoginActivity extends AppCompatActivity {
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final Button RegistButton = findViewById(R.id.login);
+        final Button loginButton = (Button)findViewById(R.id.login);
+        final Button registButton = (Button) findViewById(R.id.regist);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
@@ -114,13 +114,14 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
+
                 loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
             }
         });
 
-        RegistButton.setOnClickListener(new View.OnClickListener() {
+        registButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent1 = new Intent(LoginActivity.this, RegistActivity.class);
