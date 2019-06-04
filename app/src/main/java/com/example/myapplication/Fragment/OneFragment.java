729c192df5b869e85287ec7844f028b4363d7b5f
mia <329625584@qq.com>
2019-06-04 16:25:27 +0800

    基本注册登录+fragment+消息列表+聊天对话

--- app/src/main/java/com/example/myapplication/Fragment/OneFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/OneFragment.java
@@ -43,7 +43,6 @@ public class OneFragment extends Fragment {
         });
     }
 
-
 }
 
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 
@@ -9,7 +8,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.example.myapplication.Fragment.OneFragment;
 import com.example.myapplication.Fragment.ThreeFragment;
@@ -66,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
     }
 
-    public void onBackPressd(){
+    private void onBackPressd(){
         super.onBackPressed();
         ActivityCollector.finishAll();
     }
