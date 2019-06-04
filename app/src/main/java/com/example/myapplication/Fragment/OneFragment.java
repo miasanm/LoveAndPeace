f4186b851576260d24d15188a539932924c00590
mia <329625584@qq.com>
2019-06-04 21:57:09 +0800

    一些小问题

--- app/src/main/java/com/example/myapplication/Fragment/OneFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/OneFragment.java
@@ -1,20 +1,16 @@
 package com.example.myapplication.Fragment;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
-import com.example.myapplication.ui.login.LoginActivity;
+import com.example.myapplication.jiangxiu.PublishActivity;
 
 
 public class OneFragment extends Fragment {
@@ -31,13 +27,13 @@ public class OneFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onActivityCreated(savedInstanceState);
-        ImageView imageView = (ImageView)getActivity().findViewById(R.id.image1);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        ImageButton imageButton = (ImageButton)getActivity().findViewById(R.id.ppublic);
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
                 Toast.makeText(getActivity(), "success", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                Intent intent = new Intent(getActivity(), PublishActivity.class);
                 startActivity(intent);
             }
         });
