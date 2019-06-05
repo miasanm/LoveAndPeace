ad1b54d0fbec4b7644e0c147efcc718861fb8ecf
哟 <minisue233@163.com>
2019-06-05 09:14:51 +0800

    全部合并版

---
+++ MyApplication-master1/app/src/main/java/com/example/myapplication/jiangxiu/PlusImageActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.jiangxiu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class PlusImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plus_image);
+    }
+}
